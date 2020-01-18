be08a1807fd43a76f6cabac98fbaf551149cab67
Noah <dupasnoah@gmail.com>
2020-01-18 19:13:31 +0100

    last maj

--- exercice_2/TicTacToe.js
+++ exercice_2/TicTacToe.js
@@ -1,141 +1,54 @@
-let Fin =  (function(){
-    return{
-        ligne : (grid, y) => 
-        {
-            let val = grid[0][y];
-            if(val != undefined){
-                for(let i=1; i<3; i++){
-                    if(val != grid[i][y]){
-                        return false; // pas alligné
-                    }
-                }
-                return true;
-            }
-        },
-        colonne : (grid, x) => 
-        {
-            let val = grid[x][0];
-            if(val != undefined){
-                for(let i=1; i<3; i++){
-                    if(val != grid[x][i]){
-                        return false; // pas alligné
-                    }
-                }
-                return true;
-            }
-        },
-        diagonale : (grid) => 
-        {
-            let val = grid[0][0];
-
-            //if val != undefined alors on teste
-            if(val != undefined){
-                for(let i=1; i<3; i++){
-                    if(val != grid[i][i]){
-                        break; // pas alligné
-                    }
-                    else if(i==2){ // alligné
-                        return true
-                    }
-                }
-            }
-
-            val = grid[0][2];
-            if(val != undefined){
-                for(let i=1; i<3; i++){
-                    if(val != grid[i][2-i]){
-                        break; // pas alligné
-                    }
-                    else if(i==2){ // alligné
-                        return true
-                    }
-                }
-            }
-
-            return false; // si pas alligné
-        },
-    }
-})();
-
-
 class TicTacToe extends Observable {
     constructor(){
-        
-        //Init de Observable
         super();
 
-        //Init CurrentPlayer
-        this.currentPlayer = 0;
+        this.grid = [];// Déclaration du tableau
+        for(let i=0; i<3; i++) this.grid[i] = [];
 
-        //Init Grid
-        this.grid = [];
-        for(let i=0; i<3; i++){
-            this.grid[i] = [];
-            for(let j=0; j<3; j++){
-                //On définit toutes les cases sur undefined
-                this.grid[i][j]=undefined;
-            }
-        }
-
+        this.reset(); // Reset pour définir le currentPlayer ainsi que pour initialiser la grid
     }
     
-    getCurrentPlayer(){
-        return this.currentPlayer;
+    switchCurrentPlayer(){
+        if(this.getCurrentPlayer()==0) this.currentPlayer = 1;
+        else this.currentPlayer = 0;
     }
 
-    getCaseState(x, y){
+    getCurrentPlayer() {
+        return this.currentPlayer;
+    }
+    
+    getCaseState(x, y) {
         return this.grid[x][y];
     }
 
-    reset(){
-        //Init CurrentPlayer
-        this.currentPlayer = 0;
-
-        //Init Grid
-        for(let i=0; i<3; i++){
-            for(let j=0; j<3; j++){
-                //On définit toutes les cases sur undefined
-                this.grid[i][j]=undefined;
-            }
+    setGridUndefined(){//Init Grid sur undefined
+        for(let i=0; i<3; i++) for(let j=0; j<3; j++){
+            this.grid[i][j]=undefined; //On définit toutes les cases sur undefined
         }
     }
 
-    play(x, y){
-        //On joue le coût et on change le joueur actuel
-        this.grid[x][y] = this.getCurrentPlayer();
+    reset(){
+        this.currentPlayer = 0;//Init CurrentPlayer
+        this.setGridUndefined();//Init Grid
+    }
 
-        if(this.getCurrentPlayer()==0) this.currentPlayer++;
-        else this.currentPlayer = 0;
+    play(x, y){//On joue le coût et on change le joueur actuel
+        this.grid[x][y] = this.getCurrentPlayer();
+        this.switchCurrentPlayer();
     }
     
     hasWinner(){
-        if(Fin.diagonale(this.grid)) return true;
-        for(let i=0; i<3; i++){
-            if(Fin.ligne(this.grid, i)) return true;
-            if(Fin.colonne(this.grid, i)) return true;
-        }
-        return false;
+        if(this.getWinner()!=undefined) return true;
+        else return false;
     }
-    
-    getWinner(){
-        if(Fin.diagonale(this.grid)) return this.getCaseState(1,1);
-        for(let i=0; i<3; i++){
-            if(Fin.ligne(this.grid, i)) return this.getCaseState(1,i);
-            if(Fin.colonne(this.grid, i)) return this.getCaseState(i,1);
-        }
-        return undefined;
+
+    getWinner() {
+        return test.Fin(this.grid);
     }
 
     isFinished(){
-        if(this.hasWinner()) return true; // joueur a gagné
-        for(let i=0; i<3; i++){
-            for(let j=0; j<3; j++){
-                if(this.grid[i][j]==undefined){
-                    return false; // case libre
-                }
-            }
-        }
-        return true; // terrain plein
+        if(this.hasWinner()) return true; // si joueur a gagné
+        return test.CaseLibre(this.grid);
     }
 
 }
